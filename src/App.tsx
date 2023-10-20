fed7504c4055bc9bcf378e6d9bbe6cb585558ca4
therohantomar <therohantomar@gmail.com>
2023-10-20 16:45:35 +0530

    Browser Router added with route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Container from "./components/Container";
+import Home from "./components/Home";
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Container />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 
 function App() {
-
-
   return (
     <>
-      helllon this is from feature01
+      <RouterProvider router={BrowserRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import {Outlet} from "react-router-dom"
+
+const Container = () => {
+  return (
+    <div>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Container
