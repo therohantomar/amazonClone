83f1ebbb5056267c15eaa72b649969456f293755
therohantomar <therohantomar@gmail.com>
2023-10-25 12:38:52 +0530

    changes made

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,14 +1,20 @@
 const All = () => {
   return (
-    <main className="container  h-full">
-      <div className="bg-[url('https://m.media-amazon.com/images/G/31/AmazonVideo/2019/MLP.jpg')] bg-no-repeat bg-clip-border  sm:bg-contain md:bg-contain lg:bg-contain  xl:bg-cover 2xl:bg-cover   h-[80vh] w-full">
-        <div className="w-[50vw] bg-gradient-to-r from-black via-black to-transparent flex flex-col h-full py-10 items-start gap-10  mx-8 bg-transparent">
-          <h1 className="text-white bg-transparent  text-5xl">Welcome to Prime Videos</h1>
-          <p className="text-white text-2xl bg-transparent">Watch the latest movies, TV shows, and award-winning Amazon Originals</p>
-          <button className="px-20 py-3 hover:bg-cyan-500 text-white bg-cyan-600 font-semibold rounded-sm ">Sign in to join</button>
+    <main className="container  min-h-screen">
+      <div className="bg-[url(https://m.media-amazon.com/images/G/31/AmazonVideo/2019/MLP.jpg)] bg-no-repeat bg-clip-border xs:bg-contain  sm:bg-contain md:bg-contain lg:bg-contain    xl:bg-cover 2xl:bg-cover 3xl:bg-cover w-full">
+        <div className="w-[50vw] bg-gradient-to-r from-black  via-black  to-transparent flex flex-col  py-10 items-start gap-10 min-h-screen  mx-8 bg-transparent">
+          <h1 className="text-white bg-transparent xs:text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-5xl 2xl:text-5xl ">Welcome to Prime Videos</h1>
+          <p className="text-white xs:text-md sm:text-lg md:text:text-lg lg:text-xl  xl:text-2xl 2xl:text-2xl bg-transparent">Watch the latest movies, TV shows, and award-winning Amazon Originals</p>
+          <button className="xs:px-4 sm:px-10 md:px-10 lg:px-20 xl:px-20 2xl:px-20  py-3 hover:bg-cyan-500 text-white bg-cyan-600 font-semibold rounded-sm ">Sign in to join</button>
         </div>
       </div>
-      <div className="bg-[url('https://m.media-amazon.com/images/G/31/AmazonVideo/2021/X-site/MLP/TVOD/TVOD_MLP_Right.jpg')] bg-no-repeat bg-clip-border sm:bg-contain md:bg-contain lg:contain  xl:bg-cover 2xl:bg-cover h-[80vh] w-full"></div>
+      <div className="bg-[url('https://m.media-amazon.com/images/G/31/AmazonVideo/2021/X-site/MLP/TVOD/TVOD_MLP_Right.jpg')]  bg-no-repeat bg-clip-border xs:bg-contain  sm:bg-contain md:bg-contain lg:bg-contain  xl:bg-cover 2xl:bg-cover 3xl:bg-cover w-full">
+      <div className="w-[50vw] bg-gradient-to-r from-black  via-black  to-transparent flex flex-col  py-10 items-start gap-10  mx-8 bg-transparent">
+          <h1 className="text-white bg-transparent xs:text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-5xl 2xl:text-5xl ">Movie rentals on prime videos </h1>
+          <p className="text-white xs:text-md sm:text-lg md:text:text-lg lg:text-xl  xl:text-2xl 2xl:text-2xl bg-transparent">Early Access to new movies, before digital Subscription</p>
+          <button className="xs:px-4 sm:px-10 md:px-10 lg:px-20 xl:px-20 2xl:px-20  py-3 hover:bg-cyan-500 text-white bg-cyan-600 font-semibold rounded-sm ">Rent Now</button>
+        </div>
+      </div>
     </main>
   );
 };
--- src/components/subcomponents/HomeDrop.tsx
+++ src/components/subcomponents/HomeDrop.tsx
@@ -2,11 +2,11 @@ import "../../css/HomeDrop.css"
 import { Link } from "react-router-dom"
 const HomeDrop = () => {
   return (
-    <div className='HomeDrop_container'>
+    <div className='fixed top-16   p-4'>
       <ul>
-        <li><Link className="bg-transparent" to="offers">All</Link></li>
-        <li><Link className="bg-transparent" to="/">Movies</Link></li>
-        <li>TV Shows</li>
+        <li className="hover:bg-gray-300 p-2 "><Link className="bg-transparent" to="offers">All</Link></li>
+        <li className="hover:bg-gray-300 p-2 "><Link className="bg-transparent" to="/">Movies</Link></li>
+        <li >TV Shows</li>
       </ul>
     </div>
   )
